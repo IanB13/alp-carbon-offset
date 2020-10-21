7deb1dde26573553b77f5d5d218b0efc7ba9b21c
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-10-21 10:41:46 +0100

    Init

---
+++ src/components/Container.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Button} from 'semantic-ui-react'
+
+const Container = () =>{
+    return(
+        <div>
+            this is your Container
+            press go to continue
+            <Button>
+                Hello
+            </Button>
+        </div>
+    )
+
+}
+
+export default Container
+
